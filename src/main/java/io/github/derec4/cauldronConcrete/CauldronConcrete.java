0ce05e32e592b126283aa2fba9f74d718f8f6cd7
Derek Chen <derex1987@gmail.com>
2025-07-06 23:15:37 -0500

    feat: startup text

--- src/main/java/io/github/derec4/cauldronConcrete/CauldronConcrete.java
+++ src/main/java/io/github/derec4/cauldronConcrete/CauldronConcrete.java
@@ -1,6 +1,9 @@
 package io.github.derec4.cauldronConcrete;
 
 import io.github.derec4.cauldronConcrete.listeners.PowderInCauldronListener;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class CauldronConcrete extends JavaPlugin {
@@ -9,6 +12,13 @@ public final class CauldronConcrete extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
         getServer().getPluginManager().registerEvents(new PowderInCauldronListener(), this);
+
+        Bukkit.getLogger().info("");
+        Bukkit.getServer().getConsoleSender().sendMessage(Component.text("  |_______|                             " +
+                "  ").color(NamedTextColor.GREEN));
+        Bukkit.getServer().getConsoleSender().sendMessage(Component.text("  | Derex |     Cauldron Concrete v1.0.0.0").color(NamedTextColor.GREEN));
+        Bukkit.getServer().getConsoleSender().sendMessage(Component.text("  |_______|     Running on " + Bukkit.getName() + " - " + Bukkit.getVersion()).color(NamedTextColor.GREEN));
+        Bukkit.getLogger().info("");
     }
 
     @Override
