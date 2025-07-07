300f391c266f7b438c115b0729f19d0bad174cef
Derek Chen <derex1987@gmail.com>
2025-07-06 23:10:34 -0500

    feat: register event

--- src/main/java/io/github/derec4/cauldronConcrete/CauldronConcrete.java
+++ src/main/java/io/github/derec4/cauldronConcrete/CauldronConcrete.java
@@ -1,5 +1,6 @@
 package io.github.derec4.cauldronConcrete;
 
+import io.github.derec4.cauldronConcrete.listeners.PowderInCauldronListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class CauldronConcrete extends JavaPlugin {
@@ -7,7 +8,7 @@ public final class CauldronConcrete extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        getServer().getPluginManager().registerEvents(new PowderInCauldronListener(), this);
     }
 
     @Override
